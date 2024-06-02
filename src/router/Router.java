02136522d9392ee52f86c5a2ac338c61d4417112
geesadbandara <20230782@iit.ac.lk>
2024-06-02 20:50:34 +0530

    Commited all the files

---
+++ src/router/Router.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package router;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author DELL
+ */
+public class Router {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Welcome Welcome1=new Welcome();
+     Welcome1.setVisible(true);
+     ImageIcon icon = new ImageIcon("E:/Project/yoo.png");
+     Welcome1.setIconImage(icon.getImage());
+     
+    }
+    
+}
